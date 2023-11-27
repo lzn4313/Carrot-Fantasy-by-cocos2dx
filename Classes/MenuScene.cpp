a0b3996bbd4ac4611392b270b28dfe2af5b49070
lzn <3118328013@qq.com>
2023-11-27 20:37:37 +0800

    更新菜单内容（动态怪物+动态云）

--- Classes/MenuScene.cpp
+++ Classes/MenuScene.cpp
@@ -39,6 +39,7 @@ bool MenuScene::init()
     }
     /*云*/
     auto cloud = Sprite::create();
+    cloud->setName("Cloud");
     //大云
     auto cloud1 = Sprite::create("/MenuScene/Cloud.PNG");
     if (cloud1 == nullptr)
@@ -47,15 +48,12 @@ bool MenuScene::init()
     }
     else
     {
-        cloud1->setPosition(Vec2(origin.x + visibleSize.width / 4,
+        cloud1->setPosition(Vec2(origin.x - cloud1->getContentSize().width,
             origin.y + visibleSize.height * 0.85));
-        Color3B c = cloud1->getColor();
-        c.r *= -0.7;
-        c.g *= -0.7;
-        c.b *= -0.7;
-        cloud1->setColor(c);
+        cloud1->setTag(1);
         cloud->addChild(cloud1);
     }
+    ////大云运动
     //小云
     auto cloud2 = Sprite::create("/MenuScene/Cloud.PNG");
     if (cloud2 == nullptr)
@@ -65,12 +63,24 @@ bool MenuScene::init()
     else
     {
         cloud2->setScale(0.5);
-        cloud2->setPosition(Vec2(origin.x + visibleSize.width * 0.85,
+        cloud2->setPosition(Vec2(origin.x + visibleSize.width + cloud2->getContentSize().width,
             origin.y + visibleSize.height * 0.8));
+        cloud2->setTag(2);
         cloud->addChild(cloud2);
     }
 
     this->addChild(cloud);
+    /*添加怪物图形*/
+    auto monster = Sprite::create("/MenuScene/FlyMonster.PNG");
+    if (monster == nullptr) {
+        problemLoading("'FlyMonster.PNG'");
+    }
+    else {
+        monster->setPosition(Vec2(origin.x + visibleSize.width / 4,
+            origin.y + visibleSize.height * 0.8));
+        monster->setName("Monster");
+        this->addChild(monster);
+    }
     /*添加萝卜图形*/
     auto carrot = Sprite::create();
     //1,添加萝卜叶子
@@ -137,6 +147,48 @@ bool MenuScene::init()
             origin.y + visibleSize.height / 2 - title->getContentSize().height / 7));
         this->addChild(title);
     }
+
+    /*开启调度器*/
+    this->scheduleUpdate();
+
+
     return true;
+}
+
+void MenuScene::update(float dt) {
+    auto visibleSize = Director::getInstance()->getVisibleSize();
+    Vec2 origin = Director::getInstance()->getVisibleOrigin();
+    /*动物的上下运动*/
+    Node* Monster = this->getChildByName("Monster");
+    static bool direction = 1;//向下运动
+    static float dx = 0;
+    if (direction == 1) {
+        Monster->setPosition(Vec2(Monster->getPosition().x, Monster->getPosition().y - 50 * dt));
+        dx += 50 * dt;
+        if (dx >= Monster->getContentSize().height / 2) {
+            direction = 0;
+            dx = 0;
+        }
+    }
+    else {
+        Monster->setPosition(Vec2(Monster->getPosition().x, Monster->getPosition().y + 50 * dt));
+        dx += 50 * dt;
+        if (dx >= Monster->getContentSize().height / 2) {
+            direction = 1;
+            dx = 0;
+        }
+    }
+
+    /*云的运动*/
+    Node* Cloud = this->getChildByName("Cloud");
+    Node* Cloud1 = Cloud->getChildByTag(1);
+    Node* Cloud2 = Cloud->getChildByTag(2);
+
+    Cloud1->setPosition(Vec2(Cloud1->getPosition().x + 30 * dt, Cloud1->getPosition().y));
+    Cloud2->setPosition(Vec2(Cloud2->getPosition().x - 50 * dt, Cloud2->getPosition().y));
+    if (Cloud1->getPosition().x >= visibleSize.width + Cloud1->getContentSize().width&& Cloud2->getPosition().x <= 0 - Cloud2->getContentSize().width) {
+        Cloud1->setPosition(Vec2(0 - Cloud1->getContentSize().width, Cloud1->getPosition().y));
+        Cloud2->setPosition(Vec2(visibleSize.width + Cloud2->getContentSize().width, Cloud2->getPosition().y));
+    }
 }
 
--- Classes/MenuScene.h
+++ Classes/MenuScene.h
@@ -10,6 +10,8 @@ public:
 
     virtual bool init();
 
+    virtual void update(float dt);
+
     CREATE_FUNC(MenuScene);
 };
 
